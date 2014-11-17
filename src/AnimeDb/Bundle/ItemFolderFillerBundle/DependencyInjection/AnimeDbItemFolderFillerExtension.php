e41f557501ec3c4a583ec199ba200896d1db06c6
Peter Gribanov <info@peter-gribanov.ru>
2014-11-17 18:09:19 +0300

    remove configuration

--- src/AnimeDb/Bundle/ItemFolderFillerBundle/DependencyInjection/AnimeDbItemFolderFillerExtension.php
+++ src/AnimeDb/Bundle/ItemFolderFillerBundle/DependencyInjection/AnimeDbItemFolderFillerExtension.php
@@ -8,20 +8,19 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
- * This is the class that loads and manages your bundle configuration
+ * DependencyInjection
  *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ * @package AnimeDb\Bundle\ItemFolderFillerBundle\DependencyInjection
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
  */
 class AnimeDbItemFolderFillerExtension extends Extension
 {
     /**
-     * {@inheritDoc}
+     * (non-PHPdoc)
+     * @see \Symfony\Component\DependencyInjection\Extension\ExtensionInterface::load()
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('parameters.yml');
         $loader->load('services.yml');
