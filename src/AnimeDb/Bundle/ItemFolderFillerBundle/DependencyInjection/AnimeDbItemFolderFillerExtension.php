2a2d5b71b40f65a99bd7051c267873588372dc14
Peter Gribanov <info@peter-gribanov.ru>
2014-11-17 18:12:44 +0300

    remove config parameters

--- src/AnimeDb/Bundle/ItemFolderFillerBundle/DependencyInjection/AnimeDbItemFolderFillerExtension.php
+++ src/AnimeDb/Bundle/ItemFolderFillerBundle/DependencyInjection/AnimeDbItemFolderFillerExtension.php
@@ -22,7 +22,6 @@ class AnimeDbItemFolderFillerExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('parameters.yml');
         $loader->load('services.yml');
     }
 }
